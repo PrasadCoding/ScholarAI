408c610db7b1c915081fae02574c016351f33314
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2025-03-23 19:01:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.markdown(
 )
 
 st.markdown(f"""
-# Welcome to ScholarAI!
+### Welcome to ScholarAI!
    
 ScholarAI is an AI-powered research assistant designed to help you with your research papers. You can upload a research paper, and ScholarAI will assist you in answering questions, summarizing key points, or analyzing the paper's content. It’s like having a virtual assistant for your academic needs!
 
