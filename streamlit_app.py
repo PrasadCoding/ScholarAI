ecc52bbf5116d15fc0b368c8b01b3d3ce09496d6
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2025-03-23 18:46:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,21 @@ col1, col2 = st.columns([4, 1])
 with col1:
     st.title("ScholarAI")
 with col2:
-    st.toggle("🌗")
+    on = st.toggle("🌗")
+
+if on:
+    theme_color = "#2C3E50"  
+else:
+    theme_color = "#ECF0F1"
+
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {theme_color};
+    }}
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
