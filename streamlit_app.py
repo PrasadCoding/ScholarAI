15a555da3f409d4a33f339d7551a319899c5f68f
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2025-03-24 15:30:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from streamlit_navigation_bar import st_navbar
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
 col1, col2 = st.columns([4, 1])
 with col1:
     st.title("ScholarAI")
@@ -24,20 +28,58 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown('#')
-st.markdown(f"""
-<div style="text-align: center;">
-    <h3 style="font-weight: bold; color: {font_color};">Welcome to ScholarAI!</h3>
-</div>
+pages = ["Home", "Library", "Tutorials", "Development", "Download"]
+styles = {
+    "nav": {
+        "background-color": "rgb(123, 209, 146)",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "border-radius": "0.5rem",
+        "color": "rgb(49, 51, 63)",
+        "margin": "0 0.125rem",
+        "padding": "0.4375rem 0.625rem",
+    },
+    "active": {
+        "background-color": "rgba(255, 255, 255, 0.25)",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
 
-<div style="text-align: center; font-size: 18px; color: {font_color};">
-    ScholarAI is an AI-powered research assistant designed to help you with your research papers. 
-    You can upload a research paper, and ScholarAI will assist you in answering questions, summarizing 
-    key points, or analyzing the paper's content. It’s like having a virtual assistant for your academic needs!
-</div>
-""", unsafe_allow_html=True)
+page = st_navbar(pages, styles=styles)
 
-st.markdown("###")
-st.markdown("""<div style="font-size: 18px; color: {font_color};">Upload a PDF</div>""", unsafe_allow_html=True)
-uploaded_file = st.file_uploader("", type="pdf")
+if page == "Home":
+    st.markdown(f"""
+    <div style="text-align: center;">
+        <h3 style="font-weight: bold; color: {font_color};">Welcome to ScholarAI!</h3>
+    </div>
 
+    <div style="text-align: center; font-size: 18px; color: {font_color};">
+        ScholarAI is an AI-powered research assistant designed to help you with your research papers. 
+        You can upload a research paper, and ScholarAI will assist you in answering questions, summarizing 
+        key points, or analyzing the paper's content. It’s like having a virtual assistant for your academic needs!
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.markdown("###")
+    st.markdown(f"""<div style="font-size: 18px; color: {font_color};">Upload a PDF</div>""", unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("", type="pdf")
+
+elif page == "Library":
+    st.write("This is the Library Page.")
+
+elif page == "Tutorials":
+    st.write("This is the Tutorials Page.")
+
+elif page == "Development":
+    st.write("This is the Development Page.")
+
+elif page == "Download":
+    st.write("This is the Download Page.")
+
+with st.sidebar:
+    st.write("Sidebar")
