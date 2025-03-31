2e7274656548973fe2b6f9799de855e2debc5f1a
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2025-03-31 19:33:37 -0400

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,31 @@
 import streamlit as st
 
+col1, col2 = st.columns([4, 1])
+with col1:
+    st.title("ScholarAI")
+with col2:
+    on = st.toggle("🌗")
+
+if on:
+    theme_color = "#2C3E50"
+    font_color = "#ECF0F1"
+else:
+    theme_color = "#ECF0F1"
+    font_color = "#2C3E50"
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {theme_color};
+        color: {font_color};
+    }}
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
